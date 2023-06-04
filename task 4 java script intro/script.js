59460025537b3da194ed45fef7f21ced8cad99a6
S. YASH RAJ <132493922+syashraj@users.noreply.github.com>
2023-06-04 14:15:57 +0530

    Add files via upload

---
+++ task 4 java script intro/script.js
@@ -0,0 +1,67 @@
+console.log("welcome guys");
+alert("thanks for your response");
+
+//variables
+var numberone=1;
+console.log(numberone);
+
+let numbertwo=2;
+console.log(numbertwo);
+
+//constants
+const pi=3.1415;
+console.log(pi);
+
+//arithematic operations
+//addition
+let number1=10;
+let number2=5;
+result=(number1+number2);
+console.log(result);
+
+//substraction
+result1=(number1-number2);
+console.log(result1);
+
+//multiplication
+result2=(number1*number2);
+console.log(result2)
+
+//divition
+result3=(number1/number2);
+console.log(result3);
+
+//reminder
+result4=(number1%number2);
+console.log(result4);
+
+//power
+result5=(number1**number2);
+console.log(result5);
+
+//increment
+result6=(number1++);
+console.log(number1);
+
+//decrement
+result7=(number2--);
+console.log(number2);
+
+//assignment operators
+//=  
+number10=10;
+console.log(number10);
+
+//==
+result8=(number1 == number2);
+console.log(result8)
+
+//===
+result9=(number1===number2);
+console.log(result9);
+
+//+=
+result10=(number1+=5);
+console.log(number1);
+
+
